4a7b6f16d6bead5666998e71ce7e00c6227e2233
Julien GUTTER <julien.gutter@gmail.com>
2024-07-30 11:37:24 +0300

    Initial commit

---
+++ src/main/java/com/openclassrooms/paymybuddyjg/PaymybuddyjgApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paymybuddyjg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyjgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddyjgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/paymybuddyjg/PaymybuddyjgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paymybuddyjg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyjgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
