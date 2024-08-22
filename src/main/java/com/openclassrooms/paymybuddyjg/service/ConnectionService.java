4fb84635c1682cd4140addd9b3c7193b12c804e8
Julien GUTTER <julien.gutter@gmail.com>
2024-08-22 17:01:54 +0300

    Création des classes services

--- src/main/java/com/openclassrooms/paymybuddyjg/service/ConnectionService.java
+++ src/main/java/com/openclassrooms/paymybuddyjg/service/ConnectionService.java
@@ -1,8 +1,33 @@
 package com.openclassrooms.paymybuddyjg.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.openclassrooms.paymybuddyjg.model.Connection;
+import com.openclassrooms.paymybuddyjg.model.Transaction;
+import com.openclassrooms.paymybuddyjg.repository.ConnectionRepository;
 
 @Service
 public class ConnectionService {
+	@Autowired
+	private ConnectionRepository connectionRepository;
+	
+	public Iterable<Connection> getConnections() {
+		return connectionRepository.findAll();
+	}
+	
+	public Optional<Connection> getConnectionById(Integer id) {
+		return connectionRepository.findById(id);
+	}
+	
+	public Connection saveConnection(Connection connection) {
+		return connectionRepository.save(connection);
+	}
+	
+	public void deleteConnectionById(Integer id) {
+		connectionRepository.deleteById(id);
+	}
 
 }
--- src/main/java/com/openclassrooms/paymybuddyjg/service/UserService.java
+++ src/main/java/com/openclassrooms/paymybuddyjg/service/UserService.java
@@ -15,11 +15,19 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	public Iterable<User> getProducts() {
+	public Iterable<User> getUsers() {
 		return userRepository.findAll();
 	}
 	
 	public Optional<User> getUserById(Integer id) {
 		return userRepository.findById(id);
+	}
+	
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	public void deleteUserById(Integer id) {
+		userRepository.deleteById(id);
 	}
 }
